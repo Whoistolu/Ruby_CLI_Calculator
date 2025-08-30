5bf90a7009def0ca0f7a86770a733af81c8dd648
Whoistolu <digittolu25@gmail.com>
2025-08-30 23:52:28 +0100

    `Remove redundant variable assignments in calculator_spec.rb and use let(:calc) { Calculator.new }`

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -1,75 +1,64 @@
 require_relative '../calculator'
 
 RSpec.describe Calculator do
+    let(:calc) { Calculator.new }
+
   describe '#add' do
     it 'adds two integers correctly' do
-      calc = Calculator.new
       expect(calc.add(2, 3)).to eq(5)
     end
 
     it 'adds integer to 0' do
-      calc = Calculator.new
       expect(calc.add(4, 0)).to eq(4)
     end
 
     it 'returns the sum of two negative numbers' do
-      calc = Calculator.new
       expect(calc.add(-6, -8)).to eq(-14)
     end
   end
 
   describe '#subtract' do
     it 'returns the difference of two numbers' do
-      calc = Calculator.new
       expect(calc.subtract(6, 3)).to eq(3)
     end
 
     it 'returns zero when subtracting the same number' do
-      calc = Calculator.new
       expect(calc.subtract(5, 5)).to eq(0)
     end
 
     it 'returns a negative when smaller minus bigger' do
-      calc = Calculator.new
       expect(calc.subtract(2, 8)).to eq(-6)
     end
   end
 
   describe '#divide' do
     it 'returns the quotient of two numbers' do
-      calc = Calculator.new
       expect(calc.divide(20, 4)).to eq(5.0)
     end
 
     it 'returns the number when divided by 1' do
-      calc = Calculator.new
       expect(calc.divide(4, 1)).to eq(4.0)
     end
 
     it 'returns a float when odd divided by even' do
-      calc = Calculator.new
       expect(calc.divide(7, 20)).to eq(0.35)
     end
 
     it 'raises error when dividing by zero' do
-      calc = Calculator.new
       expect { calc.divide(5, 0) }.to raise_error(ZeroDivisionError)
     end
   end
 
   describe '#multiply' do
     it 'returns the product of two numbers' do
-      calc = Calculator.new
       expect(calc.multiply(25, 4)).to eq(100)
     end
 
     it 'returns zero when multiplied by zero' do
-      calc = Calculator.new
       expect(calc.multiply(8, 0)).to eq(0)
     end
 
     it 'returns a negative when multiplied by a negative' do
-      calc = Calculator.new
       expect(calc.multiply(6, -3)).to eq(-18)
     end
   end
