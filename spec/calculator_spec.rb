b877f18caa851a435e351430b77d2fc9e3f3ed5e
Whoistolu <digittolu25@gmail.com>
2025-08-30 23:35:46 +0100

    Add test case for adding integer to 0

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -6,5 +6,9 @@ RSpec.describe Calculator do
             calc = Calculator.new
             expect(calc.add(2, 3)).to eq(5)
         end
+
+        it 'adds integer to 0' do
+            expect(calc.add(4, 0)).to eq(4)
+        end
     end
 end
