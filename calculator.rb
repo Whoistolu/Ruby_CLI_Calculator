f1c733d39afa54c9404ab4ac8bd5bf2009320395
Whoistolu <digittolu25@gmail.com>
2025-08-30 23:49:58 +0100

    `Resolve lint errors in  calculator.rb and added tests in calculator_spec.rb`

--- calculator.rb
+++ calculator.rb
@@ -1,15 +1,19 @@
 class Calculator
-    def add(a, b)
-        a + b
-    end
+  def add(a, b)
+    a + b
+  end
 
-    def subtract(a, b)
-        a - b
-    end
+  def subtract(a, b)
+    a - b
+  end
 
-     def divide(a, b)
-        raise ZeroDivisionError, 'Cannot divide by zero' if b == 0
+  def divide(a, b)
+    raise ZeroDivisionError, 'Cannot divide by zero' if b == 0
 
-        a.to_f / b
-    end
+    a.to_f / b
+  end
+
+  def multiply(a, b)
+    a * b
+  end
 end
