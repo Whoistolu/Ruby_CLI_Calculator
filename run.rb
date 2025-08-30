66f3388caf7007503bd814727433b453b642be8a
Whoistolu <digittolu25@gmail.com>
2025-08-30 23:55:56 +0100

    `Add user input and basic calculator functionality to run.rb`

--- run.rb
+++ run.rb
@@ -2,3 +2,33 @@ require_relative './calculator'
 
 calc = Calculator.new
 
+puts 'welcome to Ruby Calculator App'
+puts 'Enter expressions like: 2 + 3, 7 / 2, -4 * 8'
+
+print 'Enter your first number: '
+input1 = gets.chomp
+
+parts = input1.split(' ')
+
+num1 = parts[0].to_f
+operator = parts[1]
+num2 = parts[2].to_f
+
+puts "First number: #{num1}"
+puts "Operator: #{operator}"
+puts "Second number: #{num2}"
+
+result = case operator
+         when '+'
+           calc.add(num1, num2)
+         when '-'
+           calc.subtract(num1, num2)
+         when '/'
+           calc.divide(num1, num2)
+         when '*'
+           calc.multiply(num1, num2)
+         else
+           'Unknown Operator entered'
+         end
+
+puts "Result: #{result}"
