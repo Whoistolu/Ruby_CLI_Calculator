4227fd9af9e234883fa292cdfef39f3e1f64669d
Whoistolu <digittolu25@gmail.com>
2025-08-30 23:45:19 +0100

    `Add divide method to Calculator class`

--- calculator.rb
+++ calculator.rb
@@ -6,4 +6,10 @@ class Calculator
     def subtract(a, b)
         a - b
     end
+
+     def divide(a, b)
+        raise ZeroDivisionError, 'Cannot divide by zero' if b == 0
+
+        a.to_f / b
+    end
 end
